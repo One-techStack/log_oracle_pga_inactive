be8541eedb9a0274e856c777d7566ffe13685892
Sebastian Varga <sebastian@varga.ai>
2023-05-30 18:50:43 +0200

    Removed while loop

--- logrotate_monitor.sh
+++ logrotate_monitor.sh
@@ -39,21 +39,17 @@ if [[ ${#EMAIL_LIST[@]} -eq 0 ]]; then
 fi
 
 
-while true; do
-  FILE_SIZE=$(du -m "$LOG_FILE" | cut -f1) # Get file size in MB
+FILE_SIZE=$(du -m "$LOG_FILE" | cut -f1) # Get file size in MB
 
-  if (( FILE_SIZE >= SIZE_THRESHOLD )); then
-    echo "Log file size has reached the threshold." 
-    zip -r $ZIP_FILE $LOG_FILE
+if (( FILE_SIZE >= SIZE_THRESHOLD )); then
+  echo "Log file size has reached the threshold." 
+  zip -r $ZIP_FILE $LOG_FILE
 
-    # Check if zip was successful
-    if [[ $? -eq 0 ]]; then
-      send_email
-      rm $LOG_FILE
-    else
-      echo "An error occurred during zipping the log file."
-    fi
+  # Check if zip was successful
+  if [[ $? -eq 0 ]]; then
+    send_email
+    rm $LOG_FILE
+  else
+    echo "An error occurred during zipping the log file."
   fi
-
-  sleep 3600 # Wait for 1 hour before next check
 done
